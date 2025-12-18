1ac839100e2bd8ececb162bc4079b16a8b2fce86
Vivek Kumar <kr.vivekumar8@gmail.com>
2025-12-18 09:12:38 +0530

    API BASE_URL Changes.

--- app/src/main/java/com/swadratna/swadratna_staff/di/NetworkModule.kt
+++ app/src/main/java/com/swadratna/swadratna_staff/di/NetworkModule.kt
@@ -21,7 +21,7 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    const val BASE_URL = "https://api.engineerfoodie.in/"
+    const val BASE_URL = "https://api.engineerfoodies.in/"
     const val X_KEY_HEADER = "X-Key"
     const val X_KEY_VALUE = "RMWvXbJYiKDtjtCEj03iGP"
 
